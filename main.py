70b1a5ba7a525956c27a69c214f7497777a2f138
Mauricio Jiménez Miranda <kazz.deer@gmail.com>
2025-08-13 14:38:01 -0600

    Defining a Player and drawing the triangle (player) to the screen)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     print("Starting Asteroids!")
@@ -17,6 +20,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # Limiting the framerate to 60 FPS
